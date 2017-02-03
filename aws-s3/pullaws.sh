5b961d1bc406410c2fa234a871844e43fffad0cb
Joshua Arnold <arnold-jr@users.noreply.github.com>
2017-02-03 18:06:45 -0500

    Update pullaws.sh
    Scrubbing PI

--- aws-s3/pullaws.sh
+++ aws-s3/pullaws.sh
@@ -6,4 +6,4 @@ hadoop distcp \
 -Dfs.s3n.awsAccessKeyId=$AWS_ACCESS_KEY_ID \
 -Dfs.s3n.awsSecretAccessKey=$AWS_SECRET_ACCESS_KEY \
 s3n://datasets.elasticmapreduce/ngrams/books/20090715/eng-us-all/ \
-hdfs:///user/arnoldjr/eng-us-all
+hdfs:///user/$USER/eng-us-all
